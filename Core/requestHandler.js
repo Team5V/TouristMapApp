578195fb3164c08e0cd79ad350f6d68e8600109b
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-10-06 15:05:35 +0300

    Ajax handler 'Please Load Gif'

--- Core/requestHandler.js
+++ Core/requestHandler.js
@@ -1,5 +1,12 @@
 //handles all out of local navigation and data requests aka ajax manager
 
-const requestHandlerModule = () => {
-
-};
+const requestHandlerModule = (() => {
+  $(document).on({
+      ajaxStart: () => {
+          $body.addClass("loading");
+      },
+      ajaxStop: () => {
+          $body.removeClass("loading");
+      }
+  });
+});
