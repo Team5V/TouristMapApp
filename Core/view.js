eca69ac2c56712e77e8c5e7ed23367e5eff978bf
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-10-06 22:12:44 +0300

    Added advanced scrolling javascript part

--- Core/view.js
+++ Core/view.js
@@ -5,12 +5,12 @@ const viewModule = () => {
   //the two changable things
   const mainContainer = $('main');
   const sideNavigation = $('side');
-    
+
   //handles navigation clicks
   const navigate = (target) => {
     controller.handleEvent(target); // ??? ot kude da doide controllera i kak da se setne
   };
-  
+
   //handels update of the ui
   const update = (dataModel) => {
     mainContainer.append(dataModel.getMainData());
@@ -30,3 +30,45 @@ const viewModule = () => {
 
   return{navigate, update};
 };
+
+
+// Beautiful scrolling
+
+// Hide Header on on scroll down
+let didScroll;
+let lastScrollTop = 0;
+let delta = 5;
+let navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    let scrollTop = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - scrollTop) <= delta {
+          return;
+        }
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (scrollTop > lastScrollTop && scrollTop > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(scrollTop + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = scrollTop;
+}
