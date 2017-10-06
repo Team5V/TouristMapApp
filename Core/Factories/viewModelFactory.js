2c38a90245f0f9c5f10e9b7ad9f38de64e6d07bb
georgievlb <georgiev.lb@gmail.com>
2017-10-06 14:41:14 +0300

    Added groupModel to viewModelFactory

--- Core/Factories/viewModelFactory.js
+++ Core/Factories/viewModelFactory.js
@@ -1,5 +1,19 @@
 //CREATES AND RETURNS OUR HTML LAYOUTS TO BE POPULATED WITH DATA
 
 const viewModelFactoryModule = () => {
-  
+    
+    const groupModel = () => {
+        $( "#datePicker" ).datepicker();
+
+        return `Изберете Маршрут ----<br />
+        <button id="route1">Маршрут 1</button>
+        <button id="route2">Маршрут 2</button>
+        <button id="route3">Маршрут 3</button>
+        <br />
+        Изберете Дата ----<br />
+        <input type="text" id="datePicker" placeholder="Click to pick date" />
+        `
+    };
+
+    
 };
