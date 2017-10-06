4c57d764bc67e038d302de20122db77e0e1b5c27
georgievlb <georgiev.lb@gmail.com>
2017-10-06 17:42:26 +0300

    Added routesModel to viewModelFactory

--- Core/Factories/viewModelFactory.js
+++ Core/Factories/viewModelFactory.js
@@ -2,17 +2,27 @@
 
 const viewModelFactoryModule = () => {
     
+    const routesModel = () => {
+        
+        return `<p id="title"></p><br />
+                <button id="route1"></button>
+                <button id="route2"></button>
+                <button id="route3"></button>
+                <br />
+                `
+    };
+
     const groupModel = () => {
         $( "#datePicker" ).datepicker();
 
         return `Изберете Маршрут ----<br />
-        <button id="route1">Маршрут 1</button>
-        <button id="route2">Маршрут 2</button>
-        <button id="route3">Маршрут 3</button>
-        <br />
-        Изберете Дата ----<br />
-        <input type="text" id="datePicker" placeholder="Click to pick date" />
-        `
+                <button id="route1">Маршрут 1</button>
+                <button id="route2">Маршрут 2</button>
+                <button id="route3">Маршрут 3</button>
+                <br />
+                Изберете Дата ----<br />
+                <input type="text" id="datePicker" placeholder="Click to pick date" />
+                `
     };
 
     
