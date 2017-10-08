fc8229fbbb75b7068861f6569131c4e919abc3a3
georgievlb <georgiev.lb@gmail.com>
2017-10-08 15:06:42 +0300

    Added main and side models

--- Core/Factories/htmlTemplateFactory.js
+++ Core/Factories/htmlTemplateFactory.js
@@ -1,23 +1,38 @@
 //CREATES AND RETURNS OUR HTML LAYOUTS TO BE POPULATED WITH DATA
 
 const htmlTemplateFactoryModule = () => {
+    const mainObj = () => {
+        const mainObject = {
+            mainContent: $('main'),
+        }
+        mainObject.mainContent.append(`<div class="mainTitle"></div><br />
+                                       <div class="mainBody1"></div><br />
+                                       <div class="mainBody2"></div><br />
+                                       <div class="mainDescription"></div><br />
+                                      `);
+
+        return mainObject;
+    }
+
+    const sideObj = () => {
+        const sideObject = {
+            sideContent: $('aside')
+        }
+        sideObject.sideContent.append(`<div class="sideTitle"></div><br />
+                                       <div class="sideBody1"></div><br />
+                                       <div class="sideBody2"></div><br />
+                                       <div class="sideDescription"></div><br />
+                                      `)
+
+        return sideObject;
+    }
+
+        return {
+            mainObj,
+            sideObj
+        }
     
-        let mainObj = {
-            body: $('#main'),
-        };
-    
-        let sideObj = {
-            body: $('#aside'),
-        };
-    
-        let fullHtmlObj = Object.assign({}, mainObj, sideObj);
-    
-    return fullHtmlObj;
-};
-    
-    const modelsTemplates = () => {
-    
-        const routesModel = (someObj) => {
+        /*const routesModel = (someObj) => {
             
             return  someObj.assign({},
                     `<p id="title"></p><br />
@@ -36,5 +51,5 @@ const htmlTemplateFactoryModule = () => {
     
         const aboutUs = () => {
             //tuka shte sme nie
-        };
-    };
+        };*/
+};
