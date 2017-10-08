5de7cac61d6e79ec66a287ce0a9ff78c1832d863
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-10-08 23:10:59 +0300

    Scrolling nav bar semi-complete

--- Core/initialize.js
+++ Core/initialize.js
@@ -1,8 +1,24 @@
-$(document).on({
-  ajaxStart: () => {
-    $body.addClass("loading");
-  },
-  ajaxStop: () => {
-    $body.removeClass("loading");
-  }
+$(document).ready(function(){
+   let nav = $('.nav');
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 136) {
+			nav.addClass("f-nav");
+		} 
+    else {
+			nav.removeClass("f-nav");
+		}
+	});
+
+//Ajax Modal loader
+  $body = $("body");
+
+  $(document).on({
+    ajaxStart: () => {
+      $body.addClass("loading");
+    },
+    ajaxStop: () => {
+      $body.removeClass("loading");
+    }
+  });
 });
